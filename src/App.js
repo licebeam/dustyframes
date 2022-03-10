208e82520100498452089bf72c6e6690030bb499
Dustin Walker! <dustinwalkerart@gmail.com>
2022-03-10 19:44:58 +0900

    small QOL changes, still need to fix tiny bugs with times

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   const [videoLen, setVideoLen] = useState(0);
   const [currentPoS, setCurrentPos] = useState(0);
   //values for caluclating time
-  const [startFrame, setStartFrame] = useState(0);
-  const [endFrame, setEndFrame] = useState(0);
+  const [startFrame, setStartFrame] = useState(null);
+  const [endFrame, setEndFrame] = useState(null);
   // position seems to be 2 frames instead of one when moving frame by frame
   const changeURL = (value) => {
     setCurrentPos(0);
@@ -51,15 +51,20 @@ function App() {
             <Button onClick={() => setStartFrame(Math.floor(currentPoS * 60))}>Set START Frame</Button>      
             <span>Frame: {startFrame}</span>
           </div>
+          <Button onClick={() => setStartFrame(null)}>Clear Start</Button>
           <div style={{display: 'flex', flexDirection: 'column', flex: 1, textAlign: 'center'}}>
             <Button onClick={() => setEndFrame(Math.floor(currentPoS * 60))}>Set END Frame</Button>
             <span>Frame: {endFrame}</span>
           </div>
+          <Button onClick={() => setEndFrame(null)}>Clear End</Button>
         </div>
       </div>
-      <span>TOTAL FRAMES = {(endFrame - startFrame)}</span>
-      <span style={{fontSize: 20, fontWeight: 'bold'}}>FINAL TIME = {moment().startOf('day').seconds(((endFrame / 60) - (startFrame / 60))).format('HH:mm:ss')}.{getMs()}</span>
-      <span style={{fontSize: 12, color: 'red'}}> The frame count and time you may discover via Dusty Frames may not be 100% accurate to the millisecond</span>
+      <span>TOTAL FRAMES COUNTED = {(endFrame - startFrame) > 0 ? (endFrame - startFrame) : 0}</span>
+      <span style={{height: 10, fontSize: 10, color: 'brown'}}>{startFrame && endFrame ? (null) : 'Please set both start and end frames'}</span>
+      <span style={{fontSize: 30, fontWeight: 'bold', color: startFrame && endFrame ? 'green' : 'red', width: 400}}>FINAL TIME = {moment().startOf('day').seconds(((endFrame / 60) - (startFrame / 60))).format('HH:mm:ss')}.{getMs()}</span>
+      <span style={{fontSize: 12, color: 'red'}}> The frame count and time you may receive via Dusty Frames may not be 100% accurate to the millisecond</span>
+      <span>If you are ever unsure, please use Avidemux for a more accurate frame timing</span>
+      <a href="http://avidemux.sourceforge.net/">SourceForge Download</a>
       <div style={{marginTop: 80}}>
         <span> The code in this project is Open Source and free for anyone to use and contribute to.</span>
         <a href='https://github.com/licebeam/dustyframes/tree/master'> Github Repo</a>
